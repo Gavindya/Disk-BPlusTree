dedd35c75b34726c34148a44c04bf9557038c7ab
gavindya <nuwandijayawardena94@gmail.com>
2017-11-14 17:44:48 +0530

    Signed-off-by: gavindya <nuwandijayawardena94@gmail.com>

--- scripts/upload-coverage.sh
+++ scripts/upload-coverage.sh
@@ -9,6 +9,6 @@ echo $TRAVIS_BUILD_DIR
 #for file in "${files[@]}"
 #do
  #   echo "Uploading ${file}"
-    java -cp ~/codacy-coverage-reporter-assembly-latest.jar com.codacy.CodacyCoverageReporter -l Java -r ./target/site/jacoco-aggregate/jacoco.xml
+    java -cp ~/codacy-coverage-reporter-assembly-latest.jar com.codacy.CodacyCoverageReporter -l Java -r ./target/site/jacoco/jacoco.xml
 #done
 echo "Uploading completed."
